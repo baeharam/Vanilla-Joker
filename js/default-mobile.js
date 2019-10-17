f2a9892ae336cb5024ddbeca7d3fc33414f5e8b6
bae haram <hisfedev@gmail.com>
2019-10-18 00:31:03 +0900

    Implement sticky header
    Signed-off-by: bae haram <hisfedev@gmail.com>

--- js/default-mobile.js
+++ js/default-mobile.js
@@ -1,9 +1,29 @@
-const initHeader = function initHeader() {
-  const header = document.getElementById('header');
-  const headerIcon = document.querySelector('.header__icon');
-  headerIcon.addEventListener('click', () => {
-    header.classList.toggle('active');
+const initHeaderFunction = function initHeaderFunction() {
+  const header = document.getElementById("header");
+  const headerIcon = document.querySelector(".header__icon");
+  headerIcon.addEventListener("click", () => {
+    header.classList.toggle("active");
   });
 };
 
-window.onload = () => initHeader();
+const initHeaderScroll = function initHeaderScroll() {
+  const header = document.getElementById('header');
+  window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY;
+    if(scrollTop >= 50) header.classList.add('sticky');
+    else header.classList.remove('sticky'); 
+  });
+};
+
+const preventScroll = function preventScroll() {
+  window.addEventListener('scroll', (e) => {
+    e.preventDefault();
+  });
+};
+
+window.onload = () => {
+  initHeaderFunction();
+  initHeaderScroll();
+  preventScroll();
+};
+
