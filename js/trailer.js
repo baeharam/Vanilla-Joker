836aaa82f749ae7889de2c92b031a580196d9102
bae haram <hisfedev@gmail.com>
2019-11-14 11:42:55 +0900

    Implement key carousel
    Signed-off-by: bae haram <hisfedev@gmail.com>

--- js/trailer.js
+++ js/trailer.js
@@ -1,4 +1,4 @@
-const inittrailerControl = function inittrailerControl() {
+const initPlayControl = function initPlayControl() {
   const trailerWrapper = document.querySelectorAll('.trailer__wrapper');
   const playBtns = document.querySelectorAll('.trailer__play');
   const trailers = document.querySelectorAll('.trailer__video');
@@ -16,38 +16,68 @@ const inittrailerControl = function inittrailerControl() {
   });
 };
 
-const initCarousel = function initCarousel() {
-  const trailers = document.querySelectorAll('.trailer__wrapper');
+const moveTrailer = (trailers, index) => {
+  const trailer = trailers[index];
+  if (trailer.classList.contains('previous')) {
+    trailer.classList.remove('previous');
+    const previous2 = document.querySelector('.previous2');
+    if (previous2){
+      previous2.classList.replace('previous2','previous');
+    }
+    const next = document.querySelector('.next');
+    if (next) {
+      next.classList.replace('next', 'next2');
+    }
+    trailers[index + 1].classList.add('next');
+  } else if (trailer.classList.contains('next')) {
+    trailer.classList.remove('next');
+    const next2 = document.querySelector('.next2');
+    if (next2) {
+      next2.classList.replace('next2', 'next');
+    }
+    const previous = document.querySelector('.previous');
+    if (previous) {
+      previous.classList.replace('previous', 'previous2');
+    }
+    trailers[index - 1].classList.add('previous');
+  }
+};
+
+const initTrailerClick = (trailers) => {
   trailers.forEach((trailer, index) => {
-    trailer.addEventListener('click', () => {
-      if (trailer.classList.contains('previous')) {
-        trailer.classList.remove('previous');
-        const previous2 = document.querySelector('.previous2');
-        if (previous2){
-          previous2.classList.replace('previous2','previous');
-        }
-        const next = document.querySelector('.next');
-        if (next) {
-          next.classList.replace('next', 'next2');
-        }
-        trailers[index + 1].classList.add('next');
-      } else if (trailer.classList.contains('next')) {
-        trailer.classList.remove('next');
-        const next2 = document.querySelector('.next2');
-        if (next2) {
-          next2.classList.replace('next2', 'next');
-        }
-        const previous = document.querySelector('.previous');
-        if (previous) {
-          previous.classList.replace('previous', 'previous2');
-        }
-        trailers[index - 1].classList.add('previous');
-      }
-    });
+    trailer.addEventListener('click', () => moveTrailer(trailers, index));
   });
 };
 
+const initTrailerKey = (trailers) => {
+  const trailersArray = Array.from(trailers);
+  let index = -1;
+  window.addEventListener('keydown', (e) => {
+    switch(e.key) {
+      case 'ArrowLeft':
+        const previous = trailersArray.filter((trailer) => trailer.classList.contains('previous'))[0];
+        index = trailersArray.indexOf(previous);
+        if(index === -1) break;
+        moveTrailer(trailers, index);
+        break;
+      case 'ArrowRight':
+        const next = trailersArray.filter((trailer) => trailer.classList.contains('next'))[0];
+        index = trailersArray.indexOf(next);
+        if(index === -1) break;
+        moveTrailer(trailers, index);
+      default:
+        break;
+    }
+  });
+};
+
+const initCarousel = () => {
+  const trailers = document.querySelectorAll('.trailer__wrapper');
+  initTrailerClick(trailers);
+  initTrailerKey(trailers);
+};
+
 window.onload = () => {
-  inittrailerControl();
+  initPlayControl();
   initCarousel();
 };
